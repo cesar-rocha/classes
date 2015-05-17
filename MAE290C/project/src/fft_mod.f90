be59fbe68561a39107981d1f10578b67067a83f1
Cesar B Rocha <crocha@ucsd.edu>
2015-05-17 11:41:43 -0700

    Add fftw module and tests

---
+++ MAE290C/project/src/fft_mod.f90
@@ -0,0 +1,207 @@
+! $MAE290C/project/src/fft_mod.f90
+
+module fft_mod
+
+    implicit none
+
+    include 'fftw3.f'
+
+    contains
+
+!
+!  Plan initializations
+!
+
+  ! plan fft2
+  subroutine init_plan_rfft2_ip(m,n,plan)
+
+    implicit none
+
+    integer, intent(in) :: m, n
+    integer (kind=8), intent(inout) :: plan
+
+    real (kind=8) :: x(:,:)
+
+    allocate(x(n+2,n))
+
+    ! compute forward 2d fft for real input
+    call dfftw_plan_dft_r2c_2d(plan,m,n,x,x,FFTW_EXHAUSTIVE)
+
+    deallocate(x)
+
+  end subroutine init_plan_rfft2_ip
+
+  ! plan ifft2
+  subroutine init_plan_irfft2_ip(m,n,plan)
+
+    implicit none
+
+    integer, intent(in) :: m, n
+    integer (kind=8), intent(inout) :: plan
+
+    allocate(xhat(n+2,n))
+
+    real (kind=8) :: xhat(:,:)
+
+    ! compute forward 2d fft for real input
+    call dfftw_plan_dft_c2r_2d(plan,m,n,xhat,xhat,FFTW_EXHAUSTIVE)
+
+    deallocate(xhat(n+2,n))
+
+  end subroutine init_plan_irfft2_ip
+
+! plan fft2
+subroutine init_plan_rfft2(m,n,plan)
+
+  implicit none
+
+  integer, intent(in) :: m, n
+  integer (kind=8), intent(inout) :: plan
+
+  real (kind=8), dimension(m,n) :: x
+  complex (kind=8), dimension(m/2+1,n) :: xhat
+
+  ! compute forward 2d fft for real input
+  call dfftw_plan_dft_r2c_2d(plan,m,n,x,xhat,FFTW_PATIENT)
+
+end subroutine init_plan_rfft2
+
+! plan ifft2_ip
+subroutine init_plan_irfft2(m,n,plan)
+
+  implicit none
+
+  integer, intent(in) :: m, n
+  integer (kind=8), intent(inout) :: plan
+
+  real (kind=8), dimension(m,n):: x
+  complex (kind=8), dimension(m/2+1,n) :: xhat
+
+  ! compute forward 2d fft for real input
+  call dfftw_plan_dft_c2r_2d(plan,m,n,xhat,x,FFTW_EXHAUSTIVE)
+
+end subroutine init_plan_irfft2
+
+
+!
+!   FFT calls
+!
+
+    ! fft2
+    subroutine rfft2(x,xhat,m,n,plan)
+
+        ! It computes a 2D real to complex the forward 2D fft
+        !   out-of-place
+        !
+        ! Input
+        ! =======
+        ! - x : is the array to be  transformed
+        ! - n : the size of the array x
+        ! - plan: plan of execution initialized by init_plan_fft2
+        ! Output
+        ! ========
+        ! - xhat : the complex-valued DFT of x
+
+        implicit none
+
+        integer, intent(in) :: m, n
+        real (kind=8), dimension(m,n), intent(in) :: x
+        complex (kind=8), dimension(m/2+1,n), intent(inout) :: xhat
+        integer (kind=8), intent(in) :: plan
+
+        ! compute forward 2d fft for real input
+        call dfftw_execute_dft_r2c(plan,x,xhat)
+
+    end subroutine rfft2
+
+    ! ifft2
+    subroutine irfft2(xhat,x,m,n,plan)
+
+        ! It computes a 2D complex to real the forward 2D fft
+        !   out-of-place
+        !
+        ! Input
+        ! =======
+        ! - xhat : is the array to be  transformed
+        ! - n :    the size of the array x
+        ! - plan: plan of execution initialized by init_plan_irfft
+        ! Output
+        ! ========
+        ! - x : the iDFT of xhat
+
+        implicit none
+
+        integer, intent(in) :: m,n
+        real (kind=8), dimension(m,n), intent(inout) :: x
+        complex (kind=8), dimension(m/2+1,n), intent(in) :: xhat
+        integer (kind=8), intent(in) :: plan
+
+        ! compute forward 2d fft for real input
+        call dfftw_execute_dft_c2r(plan,xhat,x)
+        x = x/n/n
+
+    end subroutine irfft2
+
+
+    ! fft2 in place
+    subroutine rfft2_ip(x,m,n,plan)
+
+        ! It computes a 2D real to complex the forward 2D fft
+        !   in-place. The output overwrite the input, and is
+        !   therefore a real array, although it contains the
+        !   information of a complex array.
+        !
+        ! Input
+        ! =======
+        ! - x : is the array to be  transformed
+        ! - (m+2,n) : the shape of the array x;
+        !               that is, the original array,
+        !                padded with two rows.
+        ! - plan : plan for the transform initialized by
+        !               init_rfft2_ip
+        ! Output
+        ! ========
+        ! - xhat : the complex-valued DFT of x
+
+        implicit none
+
+        integer, intent(in) :: m, n
+        real (kind=8), dimension(m+2,n), intent(inout) :: x
+        integer (kind=8), intent(in) :: plan
+
+        ! compute forward 2d fft for real input
+        call dfftw_execute_dft_r2c(plan,x,x)
+
+    end subroutine rfft2_ip
+
+    ! ifft2_2
+    subroutine irfft2_ip(xhat,m,n,plan)
+
+        ! It computes a 2D complex to real forward 2D fft
+        !   in-place. The output overwrites the input.
+        !
+        ! Input
+        ! =======
+        ! - xhat : is the array to be  transformed (output of rfft2_ip)
+        ! - (m+2,n) :    the shape of the array
+        !
+        ! - plan : plan for the transform initialized by
+        !               init_irfft2_ip
+        ! Output
+        ! ========
+        ! - x : the iDFT of xhat
+
+        implicit none
+
+        integer, intent(in) :: m,n
+        real (kind=8), dimension(m+2,n), intent(inout) :: xhat
+        integer (kind=8), intent(in) :: plan
+
+        ! compute forward 1d fft for real input
+        call dfftw_execute_dft_c2r(plan,xhat,xhat)
+
+        xhat = xhat/n/n
+    end subroutine irfft2_ip
+
+
+end module fft_mod
---
+++ MAE290C/project/src/test_fft.f90
@@ -0,0 +1,80 @@
+program test
+
+    ! This program showcases the use of the 2D real
+    !  DFT from FFTW
+
+    !use fft_mod, only: rfft2_ip, irfft2_ip, init_plan_rfft2
+    use fft_mod
+
+    implicit none
+
+    integer :: n, i, j, nmax
+    integer (kind=8):: plan_forward, plan_backward
+    real (kind=8), allocatable :: A(:,:), Af(:,:)
+    complex (kind=8), allocatable :: Ah(:,:)
+    real (kind=8) :: k,t2,t1,t3,t4
+    real (kind=8), parameter :: pi = acos(-1.d0)
+
+    logical :: debug
+    integer(kind=8) :: tclock1, tclock2, clock_rate
+
+    n = 512
+
+    nmax = 100000
+
+    allocate(A(n+2,n), Af(n+2,n))  ! need to pad two rows, since fortran stores
+    allocate(Ah(n/2+1,n))          !    by column
+
+    debug = .false.
+
+    call random_number(A)
+
+    ! keep old array to asses accuracy
+    Af = A
+
+    call cpu_time(t3)   ! start cpu timer
+
+    ! initialize fft plans
+    call init_plan_rfft2_ip(n,n,plan_forward)
+    call init_plan_irfft2_ip(n,n,plan_backward)
+
+    call cpu_time(t4)   ! end cpu timer
+
+
+    call cpu_time(t1)   ! start cpu timer
+
+    do j = 1,nmax
+
+        !call rfft2(A,Ah,n,n,plan_forward)
+        !call irfft2(Ah,A,n,n,plan_backward)
+
+        call rfft2_ip(A,n,n,plan_forward)
+        call irfft2_ip(A,n,n,plan_backward)
+
+    enddo
+
+    call cpu_time(t2)   ! end cpu timer
+
+
+    print 9,  t4-t3
+    9 format("Initialize plans in CPU time = ",f12.8, " seconds")
+
+    print 10, nmax, t2-t1
+    10 format("Performed ",i6.6," 2D FORWARD/BACKWARD RFFT2: CPU time = ",f12.8, " seconds")
+
+    print 11, nmax, maxval(abs(A(1:n,:)-Af(1:n,:)))
+    11 format("Absolute error after ",i6.6," 2D FORWARD/BACKWARD RFT2",1pe17.8)
+
+    print 12, nmax, maxval(abs( (A(1:n,:)-Af(1:n,:))/Af(1:n,:) ))
+    12 format("Relative error after ",i6.6," 2D FORWARD/BACKWARD RFT2",1pe17.8)
+
+
+    open(unit=20, file="A", action="write", status="replace")
+    open(unit=21, file="Af", action="write", status="replace")
+
+    do j=1,n
+        write (20,*) A(j,:)
+        write (21,*) Af(j,:)
+    enddo
+
+end program test
