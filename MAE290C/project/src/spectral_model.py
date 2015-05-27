577cc10d7f48a9dd1cfbe5a54bdb92cf8e12642e
Cesar B Rocha <crocha@ucsd.edu>
2015-05-26 18:21:37 -0700

    Now working with dealias or filter

--- MAE290C/project/src/spectral_model.py
+++ MAE290C/project/src/spectral_model.py
@@ -21,7 +21,7 @@ class BTModel(object):
     def __init__(
 
             self,
-            nx = 128,
+            nx = 256,
             ny=None,
             Lx=2*pi,                    # domain size
             Ly=None,                    # width
@@ -33,7 +33,8 @@ class BTModel(object):
             tmax=100.,              # total time of integration
             filt=True,              # spectral filter flag
             use_fftw=True,
-            ntd = 1):
+            ntd = 1,
+            use_filter=True):
 
         if ny is None: ny = nx
         if Ly is None: Ly = Lx
@@ -70,7 +71,14 @@ class BTModel(object):
         self.kappa2i[self.fnz] = self.kappa2[self.fnz]**-1
         
         # a filter
-        self.filt = spec_filt(self,cphi=0.65*pi)
+        if use_filter:
+            self.filt = spec_filt(self,cphi=0.65*pi)
+        else:
+            # if not use exponential filter,
+            #   then dealias using 2/3 rule
+            self.filt = np.ones_like(self.kappa2)
+            self.filt[self.nx/3:2*self.nx/3,:] = 0.
+            self.filt[:,self.ny/3:] = 0.
 
         # other
         self.use_fftw = use_fftw
@@ -83,7 +91,7 @@ class BTModel(object):
         self._initialize_fft()
 
         # initialize step forward
-        self._init_stepforward()
+        self._init_rk3w()
 
         # initialize vorticity
         self.set_q(np.random.randn(self.ny,self.nx))
@@ -101,14 +109,55 @@ class BTModel(object):
             self.t += self.dt
             self.ndt += 1
 
-    def _printout(self):
-        """Output some basic stats."""
-        ke = self._calc_ke()
-        cfl = self._calc_cfl()
-        print 't=%16d, cfl=%5.6f, ke=%9.9f' % (
-               self.t, cfl, ke)
-        assert cfl<1., "CFL condition violated"
+    def run_with_snapshots(self, tsnapstart=0., tsnapint=432000.):
+        """ Run the model forward until the next snapshot, then yield."""
+        
+        tsnapints = np.ceil(tsnapint/self.dt)
+        nt = np.ceil(np.floor((self.tmax-tsnapstart)/self.dt+1)/tsnapints)
+        
+        while(self.t < self.tmax):
+            self._stepforward()
+            if (self.ndt%self.twrite == 0.):
+                self._printout()
+            if self.t>=tsnapstart and (self.ndt%tsnapints)==0:
+                yield self.t    
+            self.t += self.dt
+            self.ndt += 1
+        return
 
+    def _stepforward(self):
+
+        """ march the system forward using a RK3W-theta scheme """
+ 
+        self.nl1h = -self.jacobian()
+        self.qh = (self.L1*self.qh + self.c1*self.dt*self.nl1h).copy()
+        self.qh = self.filt*self.qh
+
+        self.nl2h = self.nl1h.copy()
+        self.nl1h = -self.jacobian()
+        self.qh = (self.L2*self.qh + self.c2*self.dt*self.nl1h +\
+                self.d1*self.dt*self.nl2h).copy()
+        self.qh = self.filt*self.qh
+
+        self.nl2h = self.nl1h.copy()
+        self.nl1h = -self.jacobian()
+        self.qh = (self.L3*self.qh + self.c3*self.dt*self.nl1h +\
+                self.d2*self.dt*self.nl2h).copy()
+        self.qh = self.filt*self.qh
+
+        # AB2
+        #if self.ndt == 0:
+        #    self.nl1h = -self.dt*self.jacobian()
+        #    self.qh = self.filt*(self.qh + self.nl1h)
+        #    self.nl2h = self.nl1h.copy()
+        #else:
+        #    self.nl1h = -self.dt*self.jacobian()
+        #    self.qh = self.filt*(self.qh + 1.5* self.nl1h  -0.5 * self.nl2h)
+        #    self.nl2h = self.nl1h.copy()
+
+        # forward euler
+        #self.qh = self.qh - self.dt*self.jacobian()
+ 
     def _allocate_variables(self):
         """ Allocate variables in memory """
 
@@ -123,17 +172,12 @@ class BTModel(object):
         # streamfunction
         self.p  = np.zeros(shape_real, dtype_real)
         self.ph = np.zeros(shape_cplx, dtype_cplx)
-        # velocity (only need real version)
+        # velocity
         self.u = np.zeros(shape_real, dtype_real)
         self.v = np.zeros(shape_real, dtype_real)
-        # nonlinear-term (only need complex version)
+        # nonlinear-term
         self.nl1h = np.zeros(shape_cplx, dtype_cplx)
         self.nl2h = np.zeros(shape_cplx, dtype_cplx)
-
-
-    def _invert(self):
-        """ Compute streamfunction from vorticity """
-        self.ph = -self.kappa2i*self.qh
 
     def _initialize_fft(self):
         # set up fft functions for use later
@@ -152,6 +196,7 @@ class BTModel(object):
 
     def _init_kxky(self):
         """ Calculate wavenumbers """
+
         self.dl = 2.*pi/self.Ly
         self.dk = 2.*pi/self.Lx
         self.ll = self.dl*np.append( np.arange(0.,self.ny/2),
@@ -161,33 +206,50 @@ class BTModel(object):
         self.kj = 1j*self.k
         self.lj = 1j*self.l
 
+    def _invert(self):
+        """ Compute streamfunction from vorticity """
+        self.ph = -self.kappa2i*self.qh
+
     def set_q(self,q):
         """ Initialize vorticity """ 
+
         self.q = q
         self.qh = self.fft2(self.q)
         self._invert()
         self.ph = self.filt * self.ph
-        self.ph = self.ph/(sqrt(self._calc_ke()))
+        self.ph = self.ph/(sqrt(2.*self._calc_ke()))
         self.qh = -self.kappa2*self.ph 
 
-    # jacobian in conservative form
+    def _printout(self):
+        """Output some basic stats."""
+        ke = self._calc_ke()
+        cfl = self._calc_cfl()
+        print 't=%16d, cfl=%5.6f, ke=%9.9f' % (
+               self.t, cfl, ke)
+        assert cfl<1., "CFL condition violated"
+
     def jacobian(self):
 
         """ compute the jacobian in conservative form """
 
         self._invert()
 
+
+        # dealias
+
         self.q = self.ifft2(self.qh)
         self.u = self.ifft2(-self.lj*self.ph) 
         self.v = self.ifft2( self.kj*self.ph)
 
-        uqxh = self.kj*self.fft2(self.u*self.q)
-        vqyh = self.lj*self.fft2(self.v*self.q)
+        jach = self.kj*self.fft2(self.u*self.q) +\
+                self.lj*self.fft2(self.v*self.q)
 
-        return self.filt*(uqxh + vqyh)
+        # dealias
+
+        return jach
 
     # step forward
-    def _init_stepforward(self):
+    def _init_rk3w(self):
         """ Initialize stuff for marching scheme """
 
         self.a1, self.a2, self.a3 = 29./96., -3./40., 1./6.
@@ -196,29 +258,9 @@ class BTModel(object):
         self.d1, self.d2 = -17./60., -5./12.
 
         self.Lin = -self.nu*self.kappa2*self.dt 
-
-    def _stepforward(self):
-
-        """ march the system forward using a RK3W-theta scheme """
- 
-        self.nl1h = -self.jacobian()
-        self.qh = ( (1. + self.a1*self.Lin)/(1. - self.b1*self.Lin) )*self.qh\
-                + self.c1*self.dt*self.nl1h
-        
-        self.nl2h = self.nl1h.copy()
-        self.nl1h = -self.jacobian()
-        self.qh = ( (1. + self.a2*self.Lin)/(1. - self.b2*self.Lin) )*self.qh\
-                + self.c2*self.dt*self.nl1h + self.d1*self.dt*self.nl2h
-
-        self.nl2h = self.nl1h.copy()
-        self.nl1h = -self.jacobian()
-        self.qh = ( (1. + self.a3*self.Lin)/(1. - self.b3*self.Lin) )*self.qh\
-                + self.c3*self.dt*self.nl1h + self.d2*self.dt*self.nl2h
-
-        # test with forward euler
-        #self.qh = self.qh - self.dt*self.jacobian()
-
-
+        self.L1 = ( (1. + self.a1*self.Lin)/(1. - self.b1*self.Lin) )     
+        self.L2 = ( (1. + self.a2*self.Lin)/(1. - self.b2*self.Lin) )
+        self.L3 = ( (1. + self.a2*self.Lin)/(1. - self.b3*self.Lin) )
 
     # some diagnostics
     def _calc_cfl(self):
@@ -226,49 +268,36 @@ class BTModel(object):
             np.hstack([self.u, self.v])).max()*self.dt/self.dx
 
     def _calc_ke(self):
-        ke = .5*spec_var(self, self.kappa*self.ph)
+        ke = .5*self.spec_var(self.kappa*self.ph)
         return ke.sum()
+
+    def _calc_ens(self):
+        ens = .5*self.spec_var(self.kappa2*self.ph)
 
     def _calc_eddy_time(self):
         """ estimate the eddy turn-over time in days """
 
-        ens = .5*self.H * spec_var(self, self.wv2*self.ph)
+        ens = .5 * self.spec_var(self.kappa2*self.ph)
 
-        return 2.*pi*np.sqrt( self.H / ens ) / year
+        return 2.*pi*np.sqrt( ens**-1 )
+
+    def spec_var(self,ph):
+        """ compute variance of p from Fourier coefficients ph """
+        var_dens = 2. * np.abs(ph)**2 / (self.nx*self.ny)**2 
+        # only half of coefs [0] and [nx/2+1] due to symmetry in real fft2
+        var_dens[:,0],var_dens[:,-1] = var_dens[:,0]/2.,var_dens[:,-1]/2.
+        return var_dens.sum()
 
 
 #### OLD STUFF ###
 
-def initial_psi(self, ictype="mcwilliams84"):
-
-    if ictype == "mcwilliams84":
-        # see McWilliams JFM 1984
-
-        fk = self.kappa2 != 0
-        ckappa = np.zeros(self.kappa2.shape,dtype="complex128")
-        ckappa[fk] = 1./np.sqrt( ( self.kappa2[fk]*(1. + (self.kappa2[fk]/36.)**2) ) )
-        
-        iy,ix = self.kappa2.shape
-
-        pih = np.random.randn(iy,ix)*ckappa + 1.j*np.random.randn(iy,ix)*ckappa
-        pih[0,0] = 0.
-        
-        # normalize to have energy=1
-        KE = spec_var(self,self.kappa*pih)/2.
-        pih = pih/sqrt(KE)
-
-        assert (spec_var(self,self.kappa*pih)/2-1.)<1.e-14, "Not unit energy"
-
-    return pih
-
-
 # some off-class diagnostics 
-def spec_var(self,ph):
-    """ compute variance of p from Fourier coefficients ph """
-    var_dens = 2. * np.abs(ph)**2 / (self.nx*self.ny)**2 
-    # only half of coefs [0] and [nx/2+1] due to symmetry in real fft2
-    var_dens[:,0],var_dens[:,-1] = var_dens[:,0]/2.,var_dens[:,-1]/2.
-    return var_dens.sum()
+#def spec_var(self,ph):
+#    """ compute variance of p from Fourier coefficients ph """
+#    var_dens = 2. * np.abs(ph)**2 / (self.nx*self.ny)**2 
+#    # only half of coefs [0] and [nx/2+1] due to symmetry in real fft2
+#    var_dens[:,0],var_dens[:,-1] = var_dens[:,0]/2.,var_dens[:,-1]/2.
+#    return var_dens.sum()
 
 # spectral filter
 def spec_filt(self,cphi=0.65*pi):
@@ -277,5 +306,4 @@ def spec_filt(self,cphi=0.65*pi):
     filtr = exp(-23.6*(wvx-cphi)**4.)  
     filtr[wvx<=cphi] = 1.                   
     return filtr
-
 
