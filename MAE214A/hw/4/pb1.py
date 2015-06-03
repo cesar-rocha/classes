60f3b6f21289bd2a5bcec6975ff776cc07a97624
Cesar B Rocha <crocha@ucsd.edu>
2015-06-02 20:20:34 -0700

    Fixed mean velocity calculations

--- MAE214A/hw/4/pb1.py
+++ MAE214A/hw/4/pb1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as io
+import scipy as sp
 
 plt.close('all')
 
@@ -9,9 +10,9 @@ pb1 = io.loadmat('mae214A_SPR15_hw4/pb1.mat')
 
 # (a) and (b) u at grid point (65,iz)
 def umeans(iz):
-    uy = pb1['u'][:,iz,:].mean(axis=1)
-    ut = pb1['u'][:,iz,65]
-    uyt = pb1['u'][:,iz,:].mean().repeat(uy.size)
+    uy = pb1['u'][:,:,iz].mean(axis=1)
+    ut = pb1['u'][:,65,iz]
+    uyt = pb1['u'][:,:,iz].mean().repeat(uy.size)
     return ut,uy,uyt
 
 # plotting
@@ -63,3 +64,44 @@ plt.ylim(0,20.)
 plt.xlabel(r'$z^+$')
 plt.ylabel(r'$u^+$')
 plt.savefig('pb1d_2')
+
+# in preparation for problem 2, calculate Reynolds stresses
+def tmean(u):
+    return u.mean(axis=0)
+
+def tmean_repeat(u):
+    um = tmean(u)
+    um = um.T.repeat(100).reshape(130,130,100).T 
+    return um
+
+def prime(u):
+    return u - tmean_repeat(u)
+
+def rstress(u,v):
+    return (u*v).mean(axis=(0,1))
+
+# first compute u, v, and w prime
+u = prime(pb1['u'])
+v = prime(pb1['v'])
+w = prime(pb1['w'])
+
+# now compute Reynolds stresses as a function of z
+uu = rstress(u,u)
+vv = rstress(v,v)
+ww = rstress(w,w)
+uv = rstress(u,v)
+uw = rstress(u,w)
+vw = rstress(v,w)
+
+# and the production
+um = pb1['u'].mean(axis=(0,1))
+zc = pb1['zc'][0,:]
+dzc = np.diff(zc)
+dzc = (dzc[:-1]+dzc[1:])/2.
+
+umz = (um[2:]-um[0:-2])/dzc
+prod = -uw[1:-1]*umz
+prod = np.hstack([prod[0],prod,prod[-1]])/(uu.max())  # think about this normalization
+
+# save to disk
+np.savez('myrestress.npz',zp=zp,uu=uu,vv=vv,ww=ww,uv=uv,uw=uw,vw=vw,prod=prod)
--- MAE214A/hw/4/pb2.py
+++ MAE214A/hw/4/pb2.py
@@ -8,7 +8,7 @@ path = 'mae214A_SPR15_hw4/'
 reystress = np.loadtxt(path+'chan395.reystress')
 means = np.loadtxt(path+'chan395.means')
 re2000 = np.loadtxt(path+'Re2000.stat')
-
+myres = np.load('myrestress.npz')
 
 # reystress
 # y,  y+ , R_uu, R_vv, R_ww, R_uv, R_uw , R_vw
@@ -16,7 +16,25 @@ re2000 = np.loadtxt(path+'Re2000.stat')
 # means
 # y, y+, Umean, dUmean/dy, Wmean, dWmean/dy, Pmean
 
-fig = plt.figure(figsize=(8,12))
+
+
+fig = plt.figure(figsize=(12,8))
+plt.plot(myres['zp'],myres['uu'][:64],label=r'$<uu>$')
+plt.plot(myres['zp'],myres['ww'][:64],label=r'$<vv>$')
+plt.plot(myres['zp'],myres['vv'][:64],label=r'$<ww>$')
+plt.plot(myres['zp'],myres['uw'][:64],label=r'$<uv>$')
+plt.plot(myres['zp'],myres['uv'][:64],label=r'$<uw>$')
+plt.plot(myres['zp'],myres['vw'][:64],label=r'$<vw>$')
+plt.plot(myres['zp'],myres['prod'][:64],'--',label=r'$\mathcal{P}$') # check sign
+#plt.xlim(0.,1e3)
+plt.ylim(-2.,8.)
+lg = plt.legend(loc=1)
+plt.xlabel(r'$y^+$')
+plt.ylabel(r'Reynolds stresses')
+plt.savefig('pb2a_my')
+
+
+fig = plt.figure(figsize=(12,8))
 plt.plot(reystress[:,1],reystress[:,2],label=r'$<uu>$')
 plt.plot(reystress[:,1],reystress[:,3],label=r'$<vv>$')
 plt.plot(reystress[:,1],reystress[:,4],label=r'$<ww>$')
@@ -33,7 +51,7 @@ plt.savefig('pb2a')
 
 # Re2000
 # y/h, y+, U+, u'+, v'+, w'+, -Om_z+, om_x'+, om_y'+, om_z'+, uv'+, uw'+, vw'+, pr'+, ps'+, psto'+, p'
-fig = plt.figure(figsize=(8,12))
+fig = plt.figure(figsize=(12,8))
 plt.plot(re2000[:,1],re2000[:,3],label=r'$<uu>$')
 plt.plot(re2000[:,1],re2000[:,4],label=r'$<vv>$')
 plt.plot(re2000[:,1],re2000[:,5],label=r'$<ww>$')
@@ -48,9 +66,9 @@ plt.xlabel(r'$y^+$')
 plt.ylabel(r'Reynolds stresses')
 plt.savefig('pb2b')
 
- # Re2000
+# Re2000
 # y/h, y+, U+, u'+, v'+, w'+, -Om_z+, om_x'+, om_y'+, om_z'+, uv'+, uw'+, vw'+, pr'+, ps'+, psto'+, p'
-fig = plt.figure(figsize=(8,12))
+fig = plt.figure(figsize=(12,8))
 plt.plot(re2000[:,1],re2000[:,3],label=r'$<uu>$')
 plt.plot(re2000[:,1],re2000[:,4],label=r'$<vv>$')
 plt.plot(re2000[:,1],re2000[:,5],label=r'$<ww>$')
