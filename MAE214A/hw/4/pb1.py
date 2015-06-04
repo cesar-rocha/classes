26b3573f59247aeeb04f7bdfb5ca72bc0e9cae1b
Cesar B Rocha <crocha@ucsd.edu>
2015-06-03 20:59:56 -0700

    Finished hw4

--- MAE214A/hw/4/pb1.py
+++ MAE214A/hw/4/pb1.py
@@ -47,7 +47,7 @@ plt.savefig('pb1c')
 up_data = np.loadtxt('mae214A_SPR15_hw4/Uplus.dat')
 fig = plt.figure(figsize=(8,12))
 plt.semilogx(up_data[:,0],up_data[:,1])
-plt.semilogx(zp,-4.+12.*np.log10(zp),'k--')
+#plt.semilogx(zp,-4.+12.*np.log10(zp),'k--')
 plt.xlim(0.,1e3)
 plt.ylim(0,20.)
 plt.xlabel(r'$z^+$')
@@ -66,32 +66,41 @@ plt.ylabel(r'$u^+$')
 plt.savefig('pb1d_2')
 
 # in preparation for problem 2, calculate Reynolds stresses
-def tmean(u):
-    return u.mean(axis=0)
+def tmean(vel):
+    return vel.mean(axis=0)
 
-def tmean_repeat(u):
-    um = tmean(u)
+def tmean_repeat(vel):
+    um = tmean(vel)
     um = um.T.repeat(100).reshape(130,130,100).T 
     return um
 
-def prime(u):
-    return u - tmean_repeat(u)
+def tymean_repeat(vel):
+    um = tmean(vel).mean(axis=0)
+    um = um.T.repeat(100*130).reshape(130,130,100).T 
+    return um
 
-def rstress(u,v):
-    return (u*v).mean(axis=(0,1))
+def prime(vel):
+    return vel - tmean_repeat(vel)
+
+def rstress(vel1,vel2):
+    return (vel1*vel2).mean(axis=(0,1))
 
 # first compute u, v, and w prime
-u = prime(pb1['u'])
-v = prime(pb1['v'])
-w = prime(pb1['w'])
+up = prime(pb1['u'])
+vp = prime(pb1['v'])
+wp = prime(pb1['w'])
+
+up = pb1['u'] - tymean_repeat(pb1['u'])
+vp = pb1['v'] - tymean_repeat(pb1['v'])
+wp = pb1['w'] - tymean_repeat(pb1['w'])
 
 # now compute Reynolds stresses as a function of z
-uu = rstress(u,u)
-vv = rstress(v,v)
-ww = rstress(w,w)
-uv = rstress(u,v)
-uw = rstress(u,w)
-vw = rstress(v,w)
+uu = rstress(up,up)
+vv = rstress(vp,vp)
+ww = rstress(wp,wp)
+uv = rstress(up,vp)
+uw = rstress(up,wp)
+vw = rstress(vp,wp)
 
 # and the production
 um = pb1['u'].mean(axis=(0,1))
@@ -99,9 +108,9 @@ zc = pb1['zc'][0,:]
 dzc = np.diff(zc)
 dzc = (dzc[:-1]+dzc[1:])/2.
 
-umz = (um[2:]-um[0:-2])/dzc
+umz = (um[2:]-um[0:-2])/(2*dzc)
 prod = -uw[1:-1]*umz
-prod = np.hstack([prod[0],prod,prod[-1]])/(uu.max())  # think about this normalization
+prod = np.hstack([prod[0],prod,prod[-1]])  # think about this normalization
 
 # save to disk
 np.savez('myrestress.npz',zp=zp,uu=uu,vv=vv,ww=ww,uv=uv,uw=uw,vw=vw,prod=prod)
