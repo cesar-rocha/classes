0cd7d68b65bd792eb1404b3a77f940bdcce24721
Cesar B Rocha <crocha@ucsd.edu>
2015-10-11 10:07:47 -0700

    Adds LH95 class

---
+++ THEORY_SEMINAR/fall15/Larichev_Held/simulations/LH95.py
@@ -0,0 +1,128 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import h5py
+
+import pyqg
+import LH95_params
+
+def init_condition(model,sig=1.e-7):
+    """ White noise spectrum with amplitude sig """
+    return sig*np.random.randn(model.nz,model.nx,model.ny)
+
+def _init_save_snapshots(self,path):
+
+    self.fno = path
+
+    if not os.path.isdir(self.fno):
+        os.makedirs(self.fno)
+        os.makedirs(self.fno+"/snapshots/")
+
+def _file_exist(self, fno):
+    if os.path.exists(fno):
+        if self.overwrite:
+            os.remove(fno)
+        else: raise IOError("File exists: {0}".format(fno))
+
+
+def _save_snapshots(self, fields=['qh','u','v']):
+
+    """ Save snapshots of fields """
+
+
+    fno = self.fno + '/snapshots/{:015.0f}'.format(self.t)+'.h5'
+
+    _file_exist(self,fno)
+
+    h5file = h5py.File(fno, 'w')
+
+    for field in fields:
+        h5file.create_dataset(field, data=eval("self."+field))
+
+    h5file.close()
+
+def _save_diagnostics(self):
+
+    """ Save diagnostics """
+
+    fno = self.fno + 'diagnostics.h5'
+
+    _file_exist(self,fno)
+
+    h5file = h5py.File(fno, 'w')
+
+    for key in self.diagnostics.keys():
+        h5file.create_dataset(key, data=self.get_diagnostic(key))
+      
+    h5file.close()
+
+def _save_setup(self,):
+
+    """Save setup  """
+
+    fno = self.fno + 'setup.h5'
+
+    _file_exist(self,fno)
+
+    h5file = h5py.File(fno, 'w')
+    
+    h5file.create_dataset("grid/nx", data=(self.nx),dtype=int)
+    h5file.create_dataset("grid/ny", data=(self.ny),dtype=int)
+    h5file.create_dataset("grid/nz", data=(self.nz),dtype=int)
+    h5file.create_dataset("grid/x", data=(self.x))
+    h5file.create_dataset("grid/y", data=(self.y))
+    h5file.create_dataset("grid/H", data=(self.Hi))
+    h5file.create_dataset("grid/wv", data=self.wv)
+
+    h5file.create_dataset("basestate/U", data=self.Ubg)
+    h5file.create_dataset("basestate/V", data=self.Vbg)
+
+    h5file.create_dataset("constants/beta", data=self.f)
+    h5file.create_dataset("constants/f", data=self.beta)
+
+    h5file.close()
+
+
+#
+# Simulation
+#
+
+# the basic parameters
+params = LH95_params.LHTwoLayer()
+
+# model class
+m = pyqg.LayeredModel(nx=params.Nx, nz=params.Nz, U = np.array([params.U1,params.U2]),
+                              V=np.array([0.,0.]),L=params.L,f=params.f0,beta=params.beta,
+                              rd=params.Ld, H = np.array([params.H1,params.H2]),rek=params.rek,
+                              dt=0.001,tmax=params.tmax,twrite=params.twrite, 
+                              tavestart=params.tavestart, ntd=2, delta=params.delta,
+                              hb=params.eta*params.H2,logfile=None)
+
+m.set_q(init_condition(m, sig=1.e-1))
+
+m.overwrite = True
+
+# just for testing
+m.tmax = m.tmax
+
+ke = []
+t = []
+patho = "REFERENCE/"
+k = 1
+
+kmean = 0
+_init_save_snapshots(m,patho)
+
+for i in m.run_with_snapshots(tsnapstart=0, tsnapint=.1):
+
+    ke.append(m.ke)
+    t.append(m.t)
+    
+    if m.cfl> .3:
+        m.dt = m.dt/5.
+       
+    _save_snapshots(m)
+
+_save_diagnostics(m)
+_save_setup(m)
+
---
+++ THEORY_SEMINAR/fall15/Larichev_Held/simulations/LH95_params.py
@@ -0,0 +1,51 @@
+import numpy as np
+from numpy import pi
+from pyearth import earth
+
+# parameters
+class LHTwoLayer():
+    """ Standard parameters for ACC-like
+            2-layer simulation """
+
+    def __init__(self):
+
+        self.Nx = 256          # number of grid points
+        self.Nz = 2            # number of layers
+        self.L =  2*pi         # length scale of box    [m]
+        self.Ld = 1./50.       # deformation scale      [m]
+        self.kd = 1./self.Ld   # deformation wavenumber [m^-1]
+        self.H1 = 0.5          # upper layer thickness  [m]
+        self.H2 = 0.5          # lower layer thickness  [m]
+        self.delta = self.H1/self.H2
+
+        self.b = 10*self.Ld    # deccay scale of topographic feature [m]
+        self.A = .0            # amplitude of topographic feature h/H2 [unitless]
+
+        self.U1 = .005          # upper layer zonal velocity [m/s]
+        self.U2 = -.005         # lower layer zonal velicity [m/s]
+        self.Us = self.U1-self.U2  # vertical shear             [m/s]
+
+        self.rek = .04          # linear bottom drag coeff.  [s^-1]
+
+        self.lat = 45.          # latitude
+
+        betaplane = earth.Betaplane(lat=self.lat)
+        self.f0  = betaplane.f0
+        self.beta = 0.
+         
+        self.Ti = self.Ld/(abs(self.Us))  # estimate of most unstable e-folding time scale [s]
+
+        self.dx = self.L/(self.Nx-1)            # resolution
+        self.x = np.linspace(0.,self.L,self.Nx)
+        self.y = self.x.copy()
+        self.x,self.y = np.meshgrid(self.x,self.y)
+
+        # topography
+        self.eta = self.A*np.exp( -((self.x-self.x[self.Nx/2,self.Nx/2])/self.b)**2 
+                     -((self.y-self.y[self.Nx/2,self.Nx/2])/self.b)**2 )
+
+        self.dt = self.Ti/1000.      # time-step [s]
+        self.tmax = 4000             # simulation time [s]
+        self.twrite=200
+        self.tavestart=2000
+
